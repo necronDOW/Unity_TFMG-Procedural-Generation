f3108022076085954012395cc39eecbbd891381c
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-02-27 22:54:30 +0000

    PCG_Terrain Package changes.
    End of day one, started endless terrain implementation.

---
+++ 13458211_CGP3014M_Assessment1/Assets/Packages/PCG_Terrain/Scripts/InfTerrain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfTerrain : MonoBehaviour
+{
+    public const float maxViews = 300;
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+    int chunkSize;
+    int chunksVisible;
+
+    private void Start()
+    {
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        chunksVisible = Mathf.RoundToInt(maxViews / chunkSize);
+    }
+
+    void UpdateVisibleChunks()
+    {
+        int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+    }
+}

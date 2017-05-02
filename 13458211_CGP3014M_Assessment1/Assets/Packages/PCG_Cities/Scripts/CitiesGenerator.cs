9b18de93e6c48ba850fb023248647a83d5a189d5
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-05-02 09:59:32 +0100

    Working on city placement algorithm.

--- 13458211_CGP3014M_Assessment1/Assets/Packages/PCG_Cities/Scripts/CitiesGenerator.cs
+++ 13458211_CGP3014M_Assessment1/Assets/Packages/PCG_Cities/Scripts/CitiesGenerator.cs
@@ -15,64 +15,34 @@ public class CitiesGenerator : MonoBehaviour
         cityChunkDictionary.Add(coord, new CityChunk());
     }
 
-    private void SegmentData(float[,] data, int segmentCount)
+    private Vector2[] EvaluateData(float[,] data, int divisionCount)
     {
-        float[] suitableChunks = new float[segmentCount * segmentCount];
-        int dataSize = data.GetLength(0);
-        int segmentSize = dataSize / segmentCount;
-        int halfSegmentSize = segmentSize / 2;
+        Vector2[] suitableChunks = new Vector2[frequency];
+        int areaSize = data.GetLength(0) / divisionCount;
 
-        for (int i = 0; i < segmentCount; i++)
+        for (int x = 0; x < divisionCount; x++)
         {
-            for (int j = 0; j < segmentCount; j++)
+            for (int y = 0; y < divisionCount; y++)
             {
-                int x = i * segmentSize, y = j * segmentSize;
-                int suitableIndex = (j * segmentCount) + i;
-                float midValue = data[x + halfSegmentSize, y + halfSegmentSize];
-                float variance = ChunkVariance(data, x, y, segmentSize);
-
-                if (midValue < upperHeightCap && midValue > lowerHeightCap && variance < acceptableVariance)
-                    suitableChunks[suitableIndex] = variance;
+                int evaluateX = x * areaSize;
+                int evaluateY = y * areaSize;
             }
         }
-
-        for (int i = 0; i < frequency; i++)
-        {
-            int mostSuitable = SuitableIndex(suitableChunks);
-        }
     }
 
-    private float ChunkVariance(float[,] data, int x, int y, int searchSize)
+    private bool EvaluateHeight(float[,] data, int x, int y, int areaSize)
     {
-        float min = Mathf.Infinity;
-        float max = -Mathf.Infinity;
+        int halfArea = areaSize / 2;
+        float value = data[x + halfArea, y + halfArea];
 
-        for (int i = x; i < x + searchSize; i++)
-        {
-            for (int j = y; j < y + searchSize; j++)
-                MinMax(data[i, j], ref min, ref max);
-        }
-
-        return max - min;
+        if (value < upperHeightCap && value > lowerHeightCap)
+            return true;
+        else return false;
     }
 
-    private void MinMax(float value, ref float min, ref float max)
+    private float EvaluateHeightVariance(float[,] data, int x, int y, int areaSize)
     {
-        if (value < min) min = value;
-        else if (value > max) max = value;
-    }
 
-    private int SuitableIndex(float[] values)
-    {
-        int current = 0;
-
-        for (int i = 1; i < values.Length; i++)
-        {
-            if (values[current] == 0 || values[i] < values[current])
-                current = i;
-        }
-
-        return current;
     }
 
     public class CityChunk
